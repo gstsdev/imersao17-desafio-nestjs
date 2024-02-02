56af0bffff75bed76268707a0b4c52282d9ee1e5
Gustavo Sampaio <gustavosamirts@gmail.com>
2024-02-02 17:34:28 -0300

    chore: app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssetsModule } from './assets/assets.module';
+import { OrdersModule } from './orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Asset } from './assets/entities/asset.entity';
+import { Order } from './orders/entities/order.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sql',
+      synchronize: true,
+      logging: true,
+      entities: [Asset, Order],
+    }),
+    AssetsModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
